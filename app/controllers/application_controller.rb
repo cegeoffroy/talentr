f6dce7c695e6e853a5ca45653608ddf68aaafc21
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-27 12:51:43 +0100

    added devise setting that redirects to dashboard after logging in from any page - this is temporary - need to figure out a way to redirect to dashboard only if the referrer is a pages controller, otherwise just back to referrer as per default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
   private
 
   def skip_pundit?
