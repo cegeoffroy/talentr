124e8e0a018b2702c80e52e67f023a712300ae64
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-27 16:53:28 +0100

    changed references in models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :user
-  has_many :applications
+  has_many :job_applications
   has_many :job_keywords
   has_many :candidates, through: :applications
 end
