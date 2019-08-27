8a0f312ad24d53b37a9f79752705cacb3fefddaa
charles geoffroy <charles@dgtlpay.co>
2019-08-27 16:09:31 +0100

    add photot to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_134427) do
+ActiveRecord::Schema.define(version: 2019_08_27_141801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_134427) do
     t.integer "expires_at"
     t.boolean "expires"
     t.string "refresh_token"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
