595bdb66419c6c256045105674f6d6fa8c39703c
charles geoffroy <charles@dgtlpay.co>
2019-08-27 11:56:22 +0100

    removed only on authentification for job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :authenticate_user!
   before_action :set_job, only: [:show]
 
   def new
