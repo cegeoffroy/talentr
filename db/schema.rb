608915635b953df9d9f118862f96ef55d9a1b32c
charles geoffroy <charles@dgtlpay.co>
2019-08-28 10:54:37 +0100

    added user to candidates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_141801) do
+ActiveRecord::Schema.define(version: 2019_08_27_151107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_08_27_141801) do
     t.string "linkedin_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_candidates_on_user_id"
   end
 
   create_table "infos", force: :cascade do |t|
@@ -102,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_141801) do
 
   add_foreign_key "applications", "candidates"
   add_foreign_key "applications", "jobs"
+  add_foreign_key "candidates", "users"
   add_foreign_key "infos", "candidates"
   add_foreign_key "job_keywords", "jobs"
   add_foreign_key "job_keywords", "keywords"
