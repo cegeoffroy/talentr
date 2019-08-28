391b9ee4084b5678e8bcd40ad4ec22b91de93e44
charles geoffroy <charles@dgtlpay.co>
2019-08-28 11:52:37 +0100

    creation of index jobs - updated schema with default pending value for job_application - update dashboard to render the jobs index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def dashboard
     authorize User
+    @jobs = current_user.jobs
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_154124) do
+ActiveRecord::Schema.define(version: 2019_08_28_103317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_154124) do
     t.bigint "candidate_id"
     t.bigint "job_id"
     t.datetime "date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.integer "suitability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
