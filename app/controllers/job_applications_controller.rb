61714a36f78cf58acaf4924e535cb36f804ae421
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-08-31 19:36:23 +0100

    added helper methods to get email, website, skills and histogram for graph

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -5,30 +5,28 @@ class JobApplicationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    c = parse
-    raise
     job = Job.find(params[:job_id])
-    attachments = params[:applications][:attachments]
-    attachments.each do |attachment|
-      cloudinary_result = Cloudinary::Uploader.upload(attachment.tempfile)
-      cloudinary_url = cloudinary_result["url"]
-      ### => here loop begins for each PDF uploaded
-      # TODO: upload to cloudinary - receive a link
-      # cloudinary_url = "https://res.cloudinary.com/dqh0reqn3/image/upload/v1566987213/cvtest.pdf"
-      # result = convertapi_call(cloudinary_url)
-      # => result is now a string
-      # TODO: candidate = Parser.parse(result)
-      candidate = Candidate.new(name: Faker::Name.name,
-                                email: Faker::Internet.free_email,
-                                linkedin_url: 'https://www.linkedin.com/in/dmytrotarasenko/',
-                                attachment: cloudinary_url,
-                                user: job.user)
-      candidate.save!
-      @job_application = JobApplication.new(candidate: candidate, job: job,
-                                            date: Date.today.to_datetime,
-                                            suitability: (1..100).to_a.sample)
-      authorize @job_application
-      @job_application.save!
+    if params[:applications][:attachments]
+      attachments = params[:applications][:attachments]
+      attachments.each do |attachment|
+        cloudinary_result = Cloudinary::Uploader.upload(attachment.tempfile)
+        cloudinary_url = cloudinary_result["url"]
+        result = convertapi_call(cloudinary_url)
+        candidate = parse_linkedin_cv_from_text(result)
+        # candidate = Candidate.new(name: Faker::Name.name,
+        #                           email: Faker::Internet.free_email,
+        #                           linkedin_url: 'https://www.linkedin.com/in/dmytrotarasenko/',
+        #                           attachment: cloudinary_url,
+        #                           user: job.user)
+        candidate.save!
+        @job_application = JobApplication.new(candidate: candidate, job: job,
+                                              date: Date.today.to_datetime,
+                                              suitability: (1..100).to_a.sample)
+        authorize @job_application
+        @job_application.save!
+      end
+    else
+      puts "The upload didn't contain any attachments - sorry"
     end
     ### => Here loop ends
 
--- app/helpers/parser_helper.rb
+++ app/helpers/parser_helper.rb
@@ -1,5 +1,90 @@
 module ParserHelper
-  def parse
+  def parse_linkedin_cv_from_text
     candidate = Candidate.new
   end
+
+  def get_email(text)
+    text.match(/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)[0]
+  end
+
+  def websites(text)
+    hash = {}
+    arr = []
+    sites = text.scan(/(?<full>(\s|^)www\.(\w+\.)+\w{2,3}(\/|\w|\d)+)/)
+    sites.each do |site|
+      site = site[0]
+      site.chomp!
+      site.gsub!("\n", '')
+      arr << if site.include?('linkedin')
+               { url: site, origin: 'linkedin' }
+             elsif site.include?('github')
+               { url: site, origin: 'github' }
+             elsif site.include?('facebook')
+               { url: site, origin: 'facebook' }
+             else
+               { url: site, origin: 'unknown' }
+             end
+    end
+    hash[:websites] = arr
+    hash
+  end
+
+  def stop_word?(word, file_name)
+    # true or false reading from stop_words.txt
+    word_array = []
+    File.open(file_name, "r").each_line do |line|
+      word_array << line.chomp
+    end
+    word_array.include?(word)
+  end
+
+  def number?(word)
+    word.match?(/\d/)
+  end
+
+  def non_word_only?(word)
+    word.match?(/^\W+$/)
+  end
+
+  def exclude_word?(word, stop_words_file_name)
+    stop_word?(word, stop_words_file_name) || number?(word) || non_word_only?(word) || word.empty?
+  end
+
+  def frequency_histogram(text, stop_words_file_name)
+    histogram = Hash.new(0)
+    text.chomp.split("\n").each do |line|
+      line.chomp.split(' ').each do |word|
+        clean_word = word.gsub(/(\W$)|(\Ws$)/, "").downcase
+        histogram[clean_word] += 1 unless exclude_word?(clean_word, stop_words_file_name)
+      end
+    end
+    histogram.sort_by { |_key, value| - value }.to_h
+  end
+
+  def top_skills(text)
+    if text.index('Top Skills')
+      skills = []
+      init_position = text.index('Top Skills') + 10
+      sliced_text = text.slice(init_position..-1)
+      if sliced_text[0] == "\n"
+        final_position = sliced_text.index("\n\n")
+        skills_string = sliced_text.slice!(0..final_position).strip
+        skills_string.split("\n").each { |skill| skills << skill }
+      elsif sliced_text[0] == ' '
+        final_position = sliced_text.index("\n")
+        skills_string = sliced_text.slice!(0..final_position).strip
+        skills_string.split(' ').each { |skill| skills << skill }
+      end
+    end
+  end
+
+  # content = File.read('./dima-cv.txt')
+  # content_historgram = frequency_histogram(content, './stop-words.txt')
+  # p content_historgram
+  # email = get_email(content)
+  # p email
+  # websites = websites(content)
+  # p websites
+  # skills = top_skills(content)
+  # p skills
 end
