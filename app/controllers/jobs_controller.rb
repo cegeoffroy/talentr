e37d0985c13ad355a2a8143bc15026f637b28c9d
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-09-04 10:54:59 +0100

    added simple filter design nd functionality

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,11 +27,22 @@ class JobsController < ApplicationController
   end
 
   def show
+    @fields = []
     authorize @job
   end
 
   def filter
     @job = Job.find(params[:job_id])
+    @fields = []
+    if params[:filter]
+      s = "#{params[:filter][:variable]}---#{params[:filter][:comparator]}---#{params[:filter][:value]}"
+      @fields << s
+      if params[:filter][:hidden]
+        params[:filter][:hidden].each do |k, v|
+          @fields << v
+        end
+      end
+    end
     authorize @job
     respond_to do |format|
       format.js
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -11,7 +11,7 @@ class Candidate < ApplicationRecord
     infos.where(meta_key: 'websites')[0].meta_value[:websites]
   end
 
-  FILTERS = ['Suitability score', 'Experience', 'Experience years',
+  FILTERS = ['Full Text', 'Suitability score', 'Experience', 'Experience years',
              'Relevant Experience years', 'Education years', 'Skills']
   COMPARATORS = ['contains', 'exactly equals', '>', '<']
 end
