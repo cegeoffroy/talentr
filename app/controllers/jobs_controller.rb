f6073d20274b49e43c9bb1a6341f13127bca08ee
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-09-05 10:45:32 +0100

    saving filters works

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,24 +27,33 @@ class JobsController < ApplicationController
   end
 
   def show
-    @fields = []
+    @fields = session[:fields] || []
+    @filtered_results = @job.candidates
+    filter
     authorize @job
   end
 
   def filter
-    @job = Job.find(params[:job_id])
+    if params[:job_id]
+      @job = Job.find(params[:job_id])
+    else
+      @job = Job.find(params[:id])
+    end
     @fields = []
     results = nil
     # execute if statemment if params variables are not-empty
-    if params[:filter][:hidden] != nil
-      array = params[:filter][:hidden].values.to_a
-      array.select! { |item| item.match?(/.+---.+---.+/) }
-      hidden = array.any?
+    if params[:filter] && (!params[:filter][:variable].blank? || !params[:filter][:hidden].nil?)
+      array = []
+      if !params[:filter][:hidden].nil?
+        array = params[:filter][:hidden].values.to_a
+        array.select! { |item| item.match?(/.+---.+---.+/) }
+      end
+      hidden = array.any? || !params[:filter][:variable].blank?
     else
       hidden = false
     end
 
-    if !params[:filter][:variable].blank? || hidden
+    if hidden
       s = "#{params[:filter][:variable]}---#{params[:filter][:comparator]}---#{params[:filter][:value]}"
       @fields << s
       if params[:filter][:hidden]
@@ -53,8 +62,14 @@ class JobsController < ApplicationController
         end
       end
       results = Job.search(@job, @fields)
+    elsif session[:fields].any?
+      binding.pry
+      results = Job.search(@job, session[:fields])
     end
+    p session[:fields]
+    p results
     if !results.nil? && results.any?
+      session[:fields] = @fields
       first = results[0]
       others = results[1..-1]
       @filtered_results = first.select do |n|
@@ -70,6 +85,7 @@ class JobsController < ApplicationController
     end
     authorize @job
     respond_to do |format|
+      format.html { render :show }
       format.js
     end
   end
